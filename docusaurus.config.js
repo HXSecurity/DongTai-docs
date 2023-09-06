7cf2ea90f73f7d1007d80d69618cd0f1f398fe16
tscuite <tscuite@qq.com>
2023-09-06 16:17:07 +0800

    feat: add docker

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -42,7 +42,7 @@ const config = {
           includeCurrentVersion: true,
         },
         theme: {
-          customCss: require.resolve('./src/css/custom1.css'),
+          customCss: require.resolve('./src/css/custom.css'),
         },
       }),
     ],
